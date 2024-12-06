bf68a04045b6968369a1a45954738ccc987b0f02
Jo Hooton <40861027+jo-hooton@users.noreply.github.com>
2024-12-06 19:10:10 +0000

    allow users to exclude ingredients

--- src/components/RecipeSearch.tsx
+++ src/components/RecipeSearch.tsx
@@ -7,6 +7,7 @@ import LoadingSpinner from './LoadingSpinner';
 
 const RecipeSearch: React.FC = () => {
   const [query, setQuery] = useState<string>('');
+  const [exclude, setExclude] = useState<string>('');
   const [time, setTime] = useState<string>('');
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
@@ -35,11 +36,11 @@ const RecipeSearch: React.FC = () => {
 
   // use useCallback to memoize the debounced search function
   const debouncedSearch = useCallback(
-    debounce(async (query: string, time: string, page: number) => {
+    debounce(async (query: string, exclude: string, time: string, page: number) => {
       setLoading(true);
       try {
         const response = await axios.get('http://localhost:3000/recipes', {
-          params: { query, time, page, per: 10 },
+          params: { query, exclude, time, page, per: 10 },
         });
         if (page === 1) {
           setRecipes(response.data);
@@ -66,34 +67,42 @@ const RecipeSearch: React.FC = () => {
     const value = e.target.value;
     setQuery(value);
     setCurrentPage(1);
-    debouncedSearch(value, time, 1);
+    debouncedSearch(value, exclude, time, 1);
+  };
+
+  // update exclude and invoke debounced search
+  const handleExcludeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setExclude(value);
+    setCurrentPage(1);
+    debouncedSearch(query, value, time, 1);
   };
 
   // update time and invoke debounced search
-  const handletimeChange = (e: ChangeEvent<HTMLSelectElement>) => {
+  const handleTimeChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const value = e.target.value;
     setTime(value);
     setCurrentPage(1);
-    debouncedSearch(query, value, 1);
+    debouncedSearch(query, exclude, value, 1);
   };
 
   // fetch recipes when page or input changes
   useEffect(() => {
-    if (query || time || currentPage > 1) {
-      debouncedSearch(query, time, currentPage);
+    if (query || exclude || time || currentPage > 1) {
+      debouncedSearch(query, exclude, time, currentPage);
     }
-  }, [currentPage, query, time, debouncedSearch]);
+  }, [currentPage, query, exclude, time, debouncedSearch]);
 
   // clean up the debounced function on component unmount
   useEffect(() => {
     return () => {
-      debouncedSearch.cancel(); // Cancel any pending debounce calls
+      debouncedSearch.cancel(); // cancel any pending debounce calls
     };
   }, [debouncedSearch]);
 
   return (
     <div className='grid min-h-screen w-full'>
-      <div className='flex'>
+      <div className='flex flex-col mb-3'>
         <input
           type="text"
           value={query}
@@ -101,8 +110,15 @@ const RecipeSearch: React.FC = () => {
           onChange={handleQueryChange}
           className='h-12 w-full border py-2 px-3 mb-3'
         />
+        <input
+          type="text"
+          value={exclude}
+          placeholder="Exclude ingredients..."
+          onChange={handleExcludeChange}
+          className='h-12 w-full border py-2 px-3 mb-3'
+        />
         <div className="flex h-12 mb-3">
-          <select onChange={handletimeChange} className="border py-2 px-3">
+          <select onChange={handleTimeChange} className="border py-2 px-3">
             <option value="">No time limit</option>
             <option value="15">Under 15 minutes</option>
             <option value="30">Under 30 minutes</option>
