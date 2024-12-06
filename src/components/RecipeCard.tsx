02a50e6d4cb8368563185a10f7b025c76fb01d3f
Jo Hooton <40861027+jo-hooton@users.noreply.github.com>
2024-12-06 17:21:52 +0000

    add image lazy load

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import { useState, FC } from 'react';
+import LazyLoad from 'react-lazyload';
 import { Recipe } from '../definitions/types';
 
 interface RecipeCardProps {
   recipe: Recipe;
 }
 
-const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
+const RecipeCard: FC<RecipeCardProps> = ({ recipe }) => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const imageUrl = recipe.image ? recipe.image : 'src/assets/placeholder.png';
+
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  }
+
   return (
     <div className="border p-4">
-      <img
-        src={recipe.image ? recipe.image : 'src/assets/placeholder.png'}
-        alt={recipe.title}
-        className="w-full h-40 object-cover mb-4"
-        width="500" height="600"
-      />
+      <LazyLoad height={160} once>
+        <img
+          src={imageUrl}
+          alt={recipe.title}
+          onLoad={handleImageLoad}
+          className={`w-full h-40 object-cover mb-4 transition-opacity duration-700 ${
+            isLoaded ? 'opacity-100' : 'opacity-0'
+          }`}
+        />
+      </LazyLoad>
       <h2 className="text-xl font-bold mb-2">{recipe.title}</h2>
       <ul>
         {recipe.ingredients.slice(0, 3).map((ingredient, index) => (
